dac6aec7898a7352c32dc92d477437839dd63ee1
RLMD <raphaelalmd@gmail.com>
2023-02-03 19:39:42 -0300

    Updated cap_04_exe_01.py

--- nivelamento-programacao/capitulo-4/cap_04_exe_01.py
+++ nivelamento-programacao/capitulo-4/cap_04_exe_01.py
@@ -1,5 +1,5 @@
 #Entrada do número feita pelo usuário
-num = float(input("Digite um número: "))
+num = float(input("Digite um número.\nCaso deseje encerrar a operação, digite 0: "))
 
 soma = 0
 
@@ -7,6 +7,6 @@ soma = 0
 #Caso seja, o valor é adicionado.
 while num != 0:
     soma = soma + num
-    num = float(input("Digite um número: "))
+    num = float(input("Digite um número.\nCaso deseje encerrar a operação, digite 0: "))
 
 print(f"A soma dos números é: {soma}")
